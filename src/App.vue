bcff4750e78bc73faffca80feb9e4583f630263f
Cai <106552747+aoxijy@users.noreply.github.com>
2025-07-27 20:41:47 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 import { useFirstVisit } from './composables/first-visit'
-import { useLoginDialogs } from './composables/login-dialogs'
 import { useSetTheme } from './composables/set-theme'
 import { useSubscriptionNotify } from './composables/subscription-notify'
 import { onMounted } from 'vue'
